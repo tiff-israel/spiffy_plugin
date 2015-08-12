a216ff24541c0b76cf97d5dcbbfe568c213f80f9
Tiffany Israel <tiffany@hept.org>
2015-08-11 21:02:46 -0700

    page template body_class addition
    http://www.peterrknight.com/short-body-classes-for-page-templates-wordpress/
    Signed-off-by: Tiffany Israel <tiffany@hept.org>

--- spiffy-plugin.php
+++ spiffy-plugin.php
@@ -103,3 +103,20 @@ $classes[] = $post->post_type . '-' . $post->post_name;
 return $classes;
 }
 add_filter( 'body_class', 'add_slug_body_class' );
+
+
+
+//Page Slug Body Class
+// http://www.peterrknight.com/short-body-classes-for-page-templates-wordpress/
+function spiffy_page_template_body_classes( $classes ){
+	if( is_page() ){
+		$template = get_page_template_slug(); // returns an empty string if it's loading the default template (page.php)
+		if( $template === '' ){
+			$classes[] = 'default-page';
+		} else {
+			$classes[] = sanitize_html_class( str_replace( '.php', '', $template ) );
+		}
+	}
+	return $classes;
+}
+add_filter( 'body_class', 'spiffy_page_template_body_classes' );
